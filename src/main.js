502ff40981d805be114920a2702074a62c186e4a
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-09-25 11:05:34 +0700

    1 file modified

--- src/main.js
+++ src/main.js
@@ -1,9 +1,4 @@
-import Vue from 'vue';
+const { createApp } = require('vue');
 import App from './App.vue';
-import store from './store';
 
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+createApp(App).mount('#app');
